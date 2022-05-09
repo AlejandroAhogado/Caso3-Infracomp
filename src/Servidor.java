c83a892df082eb08a6991e2a975dcf3907e89b21
Alejandro A <a.ahogado@uniandes.edu.co>
2022-05-09 11:18:25 -0500

    Avance encriptacion/ no estable

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,10 +1,22 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
+
+
+import javax.crypto.*;
+import java.security.*;
+import javax.crypto.Cipher;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class Servidor {
 	
@@ -12,10 +24,17 @@ public class Servidor {
 	private String serverMessage; //Mensajes entrantes (recibidos) en el servidor
 	private ServerSocket ss; //Socket del servidor
 	private Socket cs; //Socket del cliente
-	private DataOutputStream salidaCliente; //Flujo de datos de salida
-	
+	private DataOutputStream salidaCliente;
+    private ObjectOutputStream salidaClienteObjeto; //Flujo de datos de salida
+	private DataInputStream entradaCliente;
+    private String mensaje;
+    private String mensajeNumero;
+    private PrivateKey privateKey;
+    private PublicKey publickey;
+
 	private int numeroClientes = 0;
 	private static int contador = 0;
+    
 	
     public Servidor(int nC) throws IOException{
     	numeroClientes = nC;
@@ -27,46 +46,91 @@ public class Servidor {
         PKT_EN_ENTREGA, PKT_ENTREGADO, PKT_DESCONOCIDO;
     }   
 
-    public void startServer()//M�todo para iniciar el servidor
-    {
-        try
-        {
+    //Creo que deberia ser el metodo run, para que funcione con cada thread
+    public void startServer()
+    { 
+
+        
+        try {
+            //Generacion de llaves publica/privada servidor
+            KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+            keygen.initialize(1024);
+            KeyPair keypair = keygen.generateKeyPair();            
+            Cipher rsaCipher = Cipher.getInstance("RSA");
+            publickey = keypair.getPublic();
+            privateKey = keypair.getPrivate();
+           
+        // para descifrar  
+         // rsaCipher.init(Cipher.DECRYPT_MODE, keypair.getPublic());
+         // byte[] mensajeDescifrado = rsaCipher.doFinal(mensajeCifrado);
+         // String mensajeDescifrado2 = new String(mensajeDescifrado, "UTF8");
+         // System.out.println(mensajeDescifrado2);
+                       
+
+
+
+            //Creacion socket del cliente y del servidor
         	ss = new ServerSocket(PUERTO);
     		cs = new Socket(); 
         	
-            System.out.println("Esperando conexion");
+            System.out.println("Esperando conexion\n");
 
             while(contador<numeroClientes) {
-            	 cs = ss.accept(); //Accept comienza el socket y espera una conexi�n desde un cliente
-            	 System.out.println("Cliente "+ contador + " en linea");
+            	 cs = ss.accept(); //Inicia el socket y espera una conexion desde un cliente
+            	 System.out.println("Cliente "+ contador + " en linea\n");
                  contador++;
             }
            
             //Se obtiene el flujo de salida del cliente para enviarle mensajes
             salidaCliente = new DataOutputStream(cs.getOutputStream());
+            salidaClienteObjeto = new ObjectOutputStream(cs.getOutputStream());
+            entradaCliente = new DataInputStream(cs.getInputStream());
 
-            //Se le envia un mensaje al cliente usando su flujo de salida
-            salidaCliente.writeUTF("Peticion recibida y aceptada");
+            //Lee mensaje del cliente
+             mensaje = entradaCliente.readUTF();
+             if(mensaje.equals("INICIO"))
+             {
+                //Enviar mensaje al cliente de ACK
+                salidaCliente.writeUTF("ACK");
+             }
+             
+             //Recibir reto del cliente
+             //Falta revisar que cuando lo recibe como lo maneja byte?
+             mensajeNumero = String.valueOf(entradaCliente.readInt());
+
+             //Encriptar reto cifrado con llave privada del servidor
+             rsaCipher.init(Cipher.ENCRYPT_MODE, keypair.getPrivate());
+             byte[] mensajeCifrado = rsaCipher.doFinal(mensajeNumero.getBytes("UTF8"));
             
+             //Enviar mensaje cifrado a el cliente
+             salidaCliente.write(mensajeCifrado);
+
+
+            //Enviar llave publica
+            // try (FileOutputStream fos = new FileOutputStream("public.key")) {
+            // fos.write(keypair.getPublic().getEncoded());
+            //}
+            salidaClienteObjeto.writeObject(keypair.getPublic());
+            salidaClienteObjeto.flush();
             
-            //Se obtiene el flujo entrante desde el cliente
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+            System.out.println(keypair.getPublic());
 
-            while((serverMessage = entrada.readLine()) != null) //Mientras haya mensajes desde el cliente
-            {
-                //Se muestra por pantalla el mensaje recibido
-                System.out.println(serverMessage);
-            }
+            
+             //Imprimir Mensaje encriptado
+             System.out.println("Mensaje cifrado: "+mensajeCifrado);
 
-            System.out.println("Fin de la conexion");
 
-            ss.close();//Se finaliza la conexi�n con el cliente
+            System.out.println("Conexion finalizada");
+
+            //terminar conexion con cliente
+            ss.close();
         }
         catch (Exception e)
         {
             System.out.println(e.getMessage());
         }
     }
+}   
+   
     
-}
 
